aecabbe23284a901326f033efacee4a93b076d51
gregmacfarlane <gregmacfarlane@gmail.com>
2022-04-05 17:30:57 -0600

    Use the correct exist 5

--- sh/get_files.sh
+++ sh/get_files.sh
@@ -5,7 +5,7 @@ set box /Users/gregmacfarlane/Library/CloudStorage/Box-Box/Macfarlane/research/m
 cp $box/slc-rhSplit-pop05-net100__2022-04-03_18-30-49_iuo/ITERS/it.1/1.events.csv.gz data/Split_1.csv.gz
 cp $box/slc-rhSplit-pop05-net100__2022-04-03_18-30-49_iuo/ITERS/it.5/5.events.csv.gz data/Split_5.csv.gz
 cp $box/slc-rhExist-pop05-net100__2022-04-02_08-52-51_nfj/ITERS/it.1/1.events.csv.gz data/Exist_1.csv.gz
-cp $box/slc-rhExist-pop05-net100__2022-04-02_08-52-51_nfj/ITERS/it.5/5.events.csv.gz data/Exist_1.csv.gz
+cp $box/slc-rhExist-pop05-net100__2022-04-02_08-52-51_nfj/ITERS/it.5/5.events.csv.gz data/Exist_5.csv.gz
 cp $box/slc-rhA-pop05-net100__2022-04-03_07-50-19_ulz/ITERS/it.1/1.events.csv.gz data/A_1.csv.gz
 cp $box/slc-rhA-pop05-net100__2022-04-03_07-50-19_ulz/ITERS/it.5/5.events.csv.gz data/A_5.csv.gz
 cp $box/slc-rhB-pop05-net100__2022-04-03_07-50-25_fgi/ITERS/it.1/1.events.csv.gz data/B_1.csv.gz
