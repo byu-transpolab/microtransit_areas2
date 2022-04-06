f38e94e7f44c975b65b60fb751a13bb53dce4a28
gregmacfarlane <gregmacfarlane@gmail.com>
2022-04-06 13:49:29 -0600

    Get scenario b outputs from new run

--- sh/get_files.sh
+++ sh/get_files.sh
@@ -8,8 +8,8 @@ cp $box/slc-rhExist-pop05-net100__2022-04-02_08-52-51_nfj/ITERS/it.1/1.events.cs
 cp $box/slc-rhExist-pop05-net100__2022-04-02_08-52-51_nfj/ITERS/it.5/5.events.csv.gz data/Exist_5.csv.gz
 cp $box/slc-rhA-pop05-net100__2022-04-03_07-50-19_ulz/ITERS/it.1/1.events.csv.gz data/A_1.csv.gz
 cp $box/slc-rhA-pop05-net100__2022-04-03_07-50-19_ulz/ITERS/it.5/5.events.csv.gz data/A_5.csv.gz
-cp $box/slc-rhB-pop05-net100__2022-04-03_07-50-25_fgi/ITERS/it.1/1.events.csv.gz data/B_1.csv.gz
-cp $box/slc-rhB-pop05-net100__2022-04-03_07-50-25_fgi/ITERS/it.5/5.events.csv.gz data/B_5.csv.gz
+cp $box/slc-rhB-pop05-net100__2022-04-05_15-36-04_nwl/ITERS/it.1/1.events.csv.gz data/B_1.csv.gz
+cp $box/slc-rhB-pop05-net100__2022-04-05_15-36-04_nwl/ITERS/it.5/5.events.csv.gz data/B_5.csv.gz
 cp $box/slc-rhC-pop05-net100__2022-04-04_01-31-04_nco/ITERS/it.1/1.events.csv.gz data/C_1.csv.gz
 #cp $box/slc-rhC-pop05-net100__2022-04-04_01-31-04_nco/ITERS/it.5/5.events.csv.gz data/C_5.csv.gz
 cp $box/slc-rhD-pop05-net100__2022-04-03_18-26-26_gcv/ITERS/it.1/1.events.csv.gz data/D_1.csv.gz
